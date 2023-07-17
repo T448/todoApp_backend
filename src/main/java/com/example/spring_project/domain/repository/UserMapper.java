74b6217de0950ae476612c87e68a4ec9ad4b11d8
T448 <tatsuda4480712@gmail.com>
2023-07-17 05:37:57 +0000

    ��テーブル名の変更

--- src/main/java/com/example/spring_project/domain/repository/UserMapper.java
+++ src/main/java/com/example/spring_project/domain/repository/UserMapper.java
@@ -13,14 +13,14 @@ import com.example.spring_project.domain.model.User;
 @Mapper
 public interface UserMapper {
     // 登録用メソッド
-    @Insert("INSERT INTO user ( ulid, email, name ) VALUES ( #{ulid}, #{email}, #{name} )")
+    @Insert("INSERT INTO users ( ulid, email, name ) VALUES ( #{ulid}, #{email}, #{name} )")
     public boolean insert(User todoAppUser);
 
     // 検索用メソッド
-    @Select("SELECT * FROM user WHERE email = #{email}")
+    @Select("SELECT * FROM users WHERE email = #{email}")
     public List<User> selectMany(String email);
 
     // 検索用メソッド
-    @Select("SELECT * FROM user WHERE email = #{email}")
+    @Select("SELECT * FROM users WHERE email = #{email}")
     public User selectOneUser(String email);
 }
