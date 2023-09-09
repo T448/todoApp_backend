14a767a146f24496d344024856043be6f93ab2c0
T448 <tatsuda4480712@gmail.com>
2023-09-09 16:06:04 +0900

    不要なコードの削除

--- src/main/java/com/example/spring_project/infrastructure/redis/repository/RedisRepository.java
+++ src/main/java/com/example/spring_project/infrastructure/redis/repository/RedisRepository.java
@@ -8,7 +8,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
-import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
--- src/main/java/com/example/spring_project/presentation/controller/Interceptor.java
+++ src/main/java/com/example/spring_project/presentation/controller/Interceptor.java
@@ -3,12 +3,10 @@ package com.example.spring_project.presentation.controller;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-import com.example.spring_project.common.exceptions.ForbiddenException;
 import com.example.spring_project.config.ApplicationProperty;
 import com.example.spring_project.domain.entity.User;
 import com.example.spring_project.domain.repository.SessionRepository;
@@ -48,9 +46,8 @@ public class Interceptor implements HandlerInterceptor {
 
         if (requestURL.equals(loginURL) == false) {
             String checkSessionResultJsonString = sessionRepository.CheckSession(sessionID);
-            // TODO : javaオブジェクトに変換する
-            ObjectMapper  objectMapper = new ObjectMapper();
-            RedisUserInfo checkSessionResult = objectMapper.readValue(checkSessionResultJsonString, RedisUserInfo.class);
+            ObjectMapper  objectMapperForRead = new ObjectMapper();
+            RedisUserInfo checkSessionResult = objectMapperForRead.readValue(checkSessionResultJsonString, RedisUserInfo.class);
 
             SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
             Date expires = dateFormat.parse(checkSessionResult.getExpires());
@@ -61,17 +58,9 @@ public class Interceptor implements HandlerInterceptor {
                     
                     RedisUserInfo updatedSessionInfo = new RedisUserInfo(checkSessionResult.getUlid(),checkSessionResult.getEmail(), checkSessionResult.getName(), refreshResult.getAccessToken(), checkSessionResult.getRefreshToken(), refreshResult.getExpires());
 
-                    // もう一度初期化
-                    ObjectMapper objectMapper1 = new ObjectMapper();
-                    objectMapper1.enable(SerializationFeature.INDENT_OUTPUT);
-                    String updatedSessionInfoJsonString = objectMapper1.writeValueAsString(updatedSessionInfo);
-                    // String[] updatedSessionInfo = new String[6];
-                    // updatedSessionInfo[0] = checkSessionResult.get(0).toString();
-                    // updatedSessionInfo[1] = checkSessionResult.get(1).toString();
-                    // updatedSessionInfo[2] = checkSessionResult.get(2).toString();
-                    // updatedSessionInfo[3] = refreshResult.getAccessToken();
-                    // updatedSessionInfo[4] = checkSessionResult.get(4).toString();
-                    // updatedSessionInfo[5] = refreshResult.getExpires();
+                    ObjectMapper objectMapperForWrite = new ObjectMapper();
+                    objectMapperForWrite.enable(SerializationFeature.INDENT_OUTPUT);
+                    String updatedSessionInfoJsonString = objectMapperForWrite.writeValueAsString(updatedSessionInfo);
 
                     sessionRepository.RefreshAccessToken(sessionID, updatedSessionInfoJsonString);
 
