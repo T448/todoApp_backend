cf03631913d21651c92510010ef0496915f9bfc1
T448 <tatsuda4480712@gmail.com>
2023-09-19 21:48:20 +0900

    🩹リファクタリングを行った
    不要なコードの削除
    
    ファイル名の変更
    
    文字列中の不要な文字の除去

--- src/main/java/com/example/spring_project/presentation/controller/HogeController.java
+++ src/main/java/com/example/spring_project/presentation/controller/HogeController.java
@@ -2,7 +2,7 @@ package com.example.spring_project.presentation.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.spring_project.presentation.HideToken;
+import com.example.spring_project.presentation.HideValue;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,14 +14,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 @RequiredArgsConstructor
 public class HogeController {
-      private final HideToken hideToken; 
+      private final HideValue hideValue; 
     // 共通処理等の動作確認用のAPI
     @PostMapping(value = "/api/hoge")
     public String hoge(@RequestBody Map<String, Object> requestBody) {
         System.out.println("hoge");
         System.out.println(requestBody.get("auth"));
         System.out.println("----------------インターセプターで取得したトークン(hoge controller)------------------");
-        System.out.println(hideToken.getHideTokenValue());
+        System.out.println(hideValue.getHideTokenValue());
         return "hoge";
     }
 }
