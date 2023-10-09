c7778613ff97845827496c41cf1ff4507524e75d
T448 <tatsuda4480712@gmail.com>
2023-10-09 12:39:27 +0900

    🩹フォーマッターの適用、@ToStringアノテーションの追加

--- src/main/java/com/example/spring_project/common/response/LoginResponse.java
+++ src/main/java/com/example/spring_project/common/response/LoginResponse.java
@@ -2,8 +2,10 @@ package com.example.spring_project.common.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
+@ToString
 @AllArgsConstructor
 public class LoginResponse {
     String sessionId;
